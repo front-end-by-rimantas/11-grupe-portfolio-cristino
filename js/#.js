93fa39564edfda92e4075117ce3e478c4747ba82
vaivucka <vaiva.svegzdaite@gmail.com>
2019-11-21 12:52:20 +0200

    smale changes

---
+++ js/#.js
@@ -0,0 +1,85 @@
+"use strict";
+
+// header
+
+// hero
+
+// about me+hobbies
+function renderHobbies (hobbiesList) {
+    let HTML = '';
+
+    for (let i=0; i<hobbies.length; i++) {
+        const hobbies = hobbiesList[i];
+
+        HTML += `<div class = "hobbies col-3">  
+                <i class="fa fa-${hobbies.icon}"></i> 
+                <p>${hobbies.title}</p>
+                </div>`;
+    }
+
+return document.querySelector('#hobbies').innerHTML = HTML;
+}
+
+// what i offer-services
+function renderServices (servicesList) {
+    let HTML ='';
+
+    for (let i=0; i<services.length; i++) {
+        const services = servicesList[i];
+
+        HTML  += `<div class = "services col-4">
+                    <div class="wrapper">
+                        <i class="fa fa-${services.icon}"></i>
+                        <p>${services.title}</p>
+                        <p>${services.p}</p>
+                    </div>
+                </div>`;
+    }
+
+return document.querySelector ('#services').innerHTML = HTML;
+}
+
+// numbers
+function renderAchievements(list) {
+    let HTML = '';
+
+    if (!Array.isArray(list)) {
+        return console.error('ERROR: list not found');
+    }
+    if (list.length === 0) {
+        return console.error('ERROR: list is empty');
+    }
+
+    for (let i = 0; i < list.length; i++) {
+        const item = list[i];
+
+        HTML += `<div class="achievement col-3">
+                    <div class="center">
+                        <i class="fa fa-${item.icon}"></i>
+                        <br>
+                        <div class="texts">
+                            <h5 class="title">${item.title}</h5>
+                            <br>
+                            <div class="number">${item.number}</div>
+                        </div>
+                    </div>
+                </div>`;
+
+    }
+
+    return document.querySelector('#achievements').innerHTML = HTML;
+}
+
+// skills
+
+// latest work
+
+// job history
+
+// services
+
+// testimonials
+
+// contact me
+
+// footer
